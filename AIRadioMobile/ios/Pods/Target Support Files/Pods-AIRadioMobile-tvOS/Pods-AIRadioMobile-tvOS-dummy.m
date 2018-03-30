c08aab4d4471da2c9d3666d9326c7923b87fbb2e
frodinm <fabrizio_1197@hotmail.com>
2018-03-30 12:58:10 -0400

    first commit

---
+++ AIRadioMobile/ios/Pods/Target Support Files/Pods-AIRadioMobile-tvOS/Pods-AIRadioMobile-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AIRadioMobile_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_AIRadioMobile_tvOS
+@end
---
+++ AIRadioMobile/ios/Pods/Target Support Files/Pods-AIRadioMobileTests/Pods-AIRadioMobileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AIRadioMobileTests : NSObject
+@end
+@implementation PodsDummy_Pods_AIRadioMobileTests
+@end
